5cfc40779d479fd078b390bf5d24651eaf77b615
nazmulhaque5297 <nazmulhaque5297@outlook.com>
2024-12-02 12:19:39 +0600

    add folders

---
+++ CrudWithAsp/Controllers/CrudController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudWithAsp
+{
+    [Route("api/CrudWithAsp")]
+    [ApiController]
+    
+    public class CrudController : ControllerBase
+    {
+        [HttpGet]
+
+        public string Get()
+        {
+            return "Hi this is Nazmul";
+        }
+    }
+}
